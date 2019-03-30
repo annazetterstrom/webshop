e18396a58e5fb7ef34eccd9cf25baae0d1d03ab4
annazetterstrom <anna.wiktoria@me.com>
2019-03-30 15:31:52 +0100

    cards

--- script/loader.js
+++ script/loader.js
@@ -1,7 +1,7 @@
 $(function(){
     //skriv ut trädslaget på Dumbledores trollstav i consolen
 
-    load('/json/wands.json', dumbledore);
+    load('/json/wands.json', createCards);
     function load(url, callback){
         xhr = new XMLHttpRequest();
         xhr.addEventListener('readystatechange', function(){
@@ -23,4 +23,31 @@ $(function(){
             //}
         }
     }
-});
+
+    function createCards(xhr){
+        let obj = JSON.parse(xhr.responseText);
+        console.log(obj.wands[0]);
+        for (let i = 0; i < obj.wands.length; i++) {
+            let card = ` <div class="container flex-cards"> 
+                            <div class="card"> 
+                                <figure class="card-header">
+                                    <img src="${
+                                        obj.wands[i].src
+                                    }" alt="wand" /> 
+                                    </figure>   
+                                    <div class="card-inner">
+                                        <h2>${obj.wands[i].owner}</h2>
+                                        <p>Wood: ${obj.wands[i].wood}<br> 
+                                            Core: ${obj.wands[i].core}</p>
+                                        
+                                    </div>
+                                    <button>Add to Cart</button>
+                            </div>
+                        </div> `;
+            $('main').append(card);  
+        }
+            
+
+    } 
+}); // ready
+
