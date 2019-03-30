952125e6345cd7063edcea9977a463f202d02e21
Tengil91 <kachiun91@gmail.com>
2019-03-30 18:28:13 +0100

    localstorage

--- script/loader.js
+++ script/loader.js
@@ -19,33 +19,66 @@ $(function(){
         for(let i=0;i<wands.length;i++){
             //if(wands[i].owner.includes("Dumbledore")){
                 //skriva ut trädslaget på dumbledores trollstav
-                console.log(wands[i].wood);
             //}
         }
     }
 
     function createCards(xhr){
         let obj = JSON.parse(xhr.responseText);
-        console.log(obj.wands[0]);
         for (let i = 0; i < obj.wands.length; i++) {
-            let card = ` <div class="container flex-cards"> 
-                            <div class="card"> 
-                                <figure class="card-header">
-                                    <img src="${
-                                        obj.wands[i].src
-                                    }" alt="wand" /> 
-                                    </figure>   
-                                    <div class="card-inner">
-                                        <h2>${obj.wands[i].owner}</h2>
-                                        <p>Wood: ${obj.wands[i].wood}<br> 
-                                            Core: ${obj.wands[i].core}</p>
+            let card = `
+                <div class="container flex-cards"> 
+                    <div class="card"> 
+                        <figure class="card-header">
+                            <img src="${
+                                obj.wands[i].src
+                            }" alt="wand" /> 
+                        </figure>   
+                        <div class="card-inner">
+                            <h2>${obj.wands[i].owner}</h2>
+                            <p>Wood: ${obj.wands[i].wood}<br> 
+                                Core: ${obj.wands[i].core}</p>
                                         
-                                    </div>
-                                    <button>Add to Cart</button>
-                            </div>
-                        </div> `;
-            $('main').append(card);  
+                        </div>
+                        <button class="addbutton" data-product-code=${obj.wands[i].code}>Add to Cart</button>
+                    </div>
+                </div> `;
+            $('main').append(card);
         }
+        $('.addbutton').on('click', function(){
+            let $this = $(this);
+            let currentProductsString = localStorage.getItem('items');
+            let currentProducts;
+
+            //det vill säga om currentProductsString är tom.
+            if(!currentProductsString){
+                currentProducts = [];
+            } else {
+                currentProducts = JSON.parse(currentProductsString);
+            }
+            let checker = true;
+            let index;
+            for(let i=0;i<currentProducts.length;i++){
+                if(currentProducts[i].item.code == ($this.attr("data-product-code"))){
+                    checker = false;
+                    index = i;
+                }
+            }
+            if(checker){
+                //lägg till {item, num} till currentProducts
+                let addItem;
+                for(let i=0;i<obj.wands.length;i++){
+                    if(obj.wands[i].code == $this.attr("data-product-code")){
+                        addItem = obj.wands[i];
+                    }
+                }
+                currentProducts.push({item: addItem, num: 1});
+            } else {
+                currentProducts[index].num += 1;
+            }
+            localStorage.setItem('items', JSON.stringify(currentProducts));
+            console.log(localStorage.getItem('items'));
+        });
             
 
     } 
