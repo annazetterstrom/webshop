a54a6b33d8aa33ea7d82fc11f992ffba1624d1a1
Tengil91 <kachiun91@gmail.com>
2019-03-30 13:22:57 +0100

    loadfunction

--- script/loader.js
+++ script/loader.js
@@ -1,14 +1,26 @@
 $(function(){
     //skriv ut trädslaget på Dumbledores trollstav i consolen
 
+    load('/json/wands.json', dumbledore);
     function load(url, callback){
         xhr = new XMLHttpRequest();
         xhr.addEventListener('readystatechange', function(){
             if(this.readyState==4 && this.status == 200){
-                callback(callback);
+                callback(this);
             }
         });
         xhr.open('GET', url, true);
         xhr.send();
+    }
+
+    function dumbledore(xhr){
+        let json = JSON.parse(xhr.responseText)
+        let wands = json.wands;
+        for(let i=0;i<wands.length;i++){
+            //if(wands[i].owner.includes("Dumbledore")){
+                //skriva ut trädslaget på dumbledores trollstav
+                console.log(wands[i].wood);
+            //}
+        }
     }
 });
