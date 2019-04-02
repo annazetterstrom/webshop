cd066b099dc80e0e55c80d8a31053f751a107aea
Tengil91 <kachiun91@gmail.com>
2019-04-02 14:39:51 +0200

    the rest

--- script/validation.js
+++ script/validation.js
@@ -1,3 +1,4 @@
+//validation of form
 $(function(){
     let $form = $('form');
     let $input = $('input');
@@ -6,16 +7,19 @@ $(function(){
         e.preventDefault()
         $('.error').remove();
         $('form div').css({"border" : "none"});
-        checkName();
-        checkMail();
-        checkAdress();
-        checkCity();
+        if(checkName()&&checkMail()&&checkAdress()&&checkCity()){
+            $('.deliveryoptions').fadeIn();
+        }
+        
     }
     function checkName(){
         if($input[0].value.length < 2){
             console.log("name error, to short");
             $($input[0]).parent().append('<p class="error">The name must be at least 2 letters long</p>');
             $($input[0]).parent().css({"border" : "red 1px solid"});
+            return false;
+        } else {
+            return true;
         }
     }
     function checkMail(){
@@ -23,6 +27,9 @@ $(function(){
             console.log("not an email");
             $($input[1]).parent().append('<p class="error">Please enter a correct email</p>');
             $($input[1]).parent().css({"border" : "red 1px solid"});
+            return false;
+        } else {
+            return true;
         }
     }
     function checkAdress(){
@@ -31,7 +38,12 @@ $(function(){
                 console.log("adress error");
                 $($input[3]).parent().append('<p class="error">I want to know where you live because reasons :D</p>');
                 $($input[3]).parent().css({"border" : "red 1px solid"});
+                return false;
+            } else {
+                return true;
             }
+        } else {
+            return true;
         }
     }
     function checkCity(){
@@ -40,7 +52,12 @@ $(function(){
                 console.log("city error, pls move");
                 $($input[4]).parent().append('<p class="error">Please enter a city to nuke</p>');
                 $($input[4]).parent().css({"border" : "red 1px solid"});
+                return false;
+            } else {
+                return true;
             }
+        } else {
+            return true;
         }
     }
 });
