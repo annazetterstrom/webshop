d4c4509727a6bd7b65a2477aa80ea9fab781ff45
annazetterstrom <anna.wiktoria@me.com>
2019-04-02 16:00:18 +0200

    test

--- script/checkoutcards.js
+++ script/checkoutcards.js
@@ -1,38 +1,37 @@
-$(function () {
+$(function(){
     createCards();
-
-    function createCards() {
+    function createCards(){
         let itemArray = JSON.parse(localStorage.getItem('items'));
         $('.container').html("");
-        if (!itemArray) {
-            $('.container').append('Din varukorg är tom!');
+        if(!itemArray){
+            $('.container').append('Your cart is empty!');
         } else {
             console.log(itemArray);
-            for (let i = 0; i < itemArray.length; i++) {
+            for(let i=0;i<itemArray.length;i++){
                 let card = `
                 <p>
-                <span>${itemArray[i].item.owner}  <span class="price">${itemArray[i].item.price}</span> Galleons</span>
-            q       <button data-product-code=${itemArray[i].item.code} class="sm-btn btn-danger removebutton">X</button>
+                    <span>${itemArray[i].item.owner}  <span class="price">${itemArray[i].item.price}</span> Galleons</span>
+                <button data-product-code=${itemArray[i].item.code} class="sm-btn btn-danger removebutton">X</button>
                 <input class="quantity" type="number" max="5" min="1" />
-           </p>
+               </p>
                   `;
                 $('.container').append(card);
             }
             let total = 0;
             let $prices = $('.price');
             console.log($prices[0]);
-            for (let i = 0; i < $prices.length; i++) {
+            for(let i=0;i<$prices.length;i++){
                 total += +$prices[i].innerText;
                 console.log($prices[i].innerText);
             }
             $('.total').text(total);
             console.log(total);
             $('.num-item').text(itemArray.length)
-            $('.addbutton').on('click', function () {
+            $('.addbutton').on('click', function(){
                 let $this = $(this);
                 let itemArray = JSON.parse(localStorage.getItem('items'));
-                for (let i = 0; i < itemArray.length; i++) {
-                    if ($this.attr("data-product-code") == itemArray[i].item.code) {
+                for(let i=0;i<itemArray.length;i++){
+                    if($this.attr("data-product-code") == itemArray[i].item.code){
                         let selectvalue = $this.siblings("select").val();
                         itemArray[i].num = selectvalue;
                         $this.siblings('div').find('.num').text(selectvalue);
@@ -41,17 +40,18 @@ $(function () {
                 }
                 localStorage.setItem('items', JSON.stringify(itemArray));
             });
-            $('.removebutton').on('click', function () {
+            $('.removebutton').on('click', function(){
                 let $this = $(this);
                 let itemArray = JSON.parse(localStorage.getItem('items'));
-                for (let i = 0; i < itemArray.length; i++) {
-                    if ($this.attr("data-product-code") == itemArray[i].item.code) {
+                for(let i=0;i<itemArray.length;i++){
+                    if($this.attr("data-product-code") == itemArray[i].item.code){
                         itemArray.splice(i, 1);
-                        if (itemArray.length === 0) {
+                        if(itemArray.length === 0){
                             localStorage.removeItem('items');
                             $('.num-item').text(itemArray.length);
                         } else {
                             localStorage.setItem('items', JSON.stringify(itemArray));
+                         
                         }
                         break;
                     }
