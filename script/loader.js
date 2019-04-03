4cb88acc635ce1b5bcc86eba51f51e0563343baf
Tengil91 <kachiun91@gmail.com>
2019-04-03 10:43:10 +0200

    .

--- script/loader.js
+++ script/loader.js
@@ -1,9 +1,8 @@
 $(function(){
-    //skriv ut trädslaget på Dumbledores trollstav i consolen
-    if(localStorage.getItem('enter')!=="true"){
-        
-    }
-    load('/json/wands.json', createCards);
+    let currentStuff = JSON.parse(localStorage.getItem('items'));
+    currentStuff = currentStuff ? currentStuff : [];
+    $('.badge').text(currentStuff.length);
+    load('./json/wands.json', createCards);
     function load(url, callback){
         xhr = new XMLHttpRequest();
         xhr.addEventListener('readystatechange', function(){
@@ -14,17 +13,6 @@ $(function(){
         xhr.open('GET', url, true);
         xhr.send();
     }
-
-    function dumbledore(xhr){
-        let json = JSON.parse(xhr.responseText)
-        let wands = json.wands;
-        for(let i=0;i<wands.length;i++){
-            //if(wands[i].owner.includes("Dumbledore")){
-                //skriva ut trädslaget på dumbledores trollstav
-            //}
-        }
-    }
-
     function createCards(xhr){
         let obj = JSON.parse(xhr.responseText);
         for (let i = 0; i < obj.wands.length; i++) {
@@ -40,9 +28,9 @@ $(function(){
                             <h2>${obj.wands[i].owner}</h2>
                             <p><b>Wood:</b> ${obj.wands[i].wood}<br> 
                                 <b>Core:</b> ${obj.wands[i].core} <br>
-                              <b> Description:</b> <br>
+                                <b> Description:</b> <br>
                                 ${obj.wands[i].description}
-                                </p>
+                            </p>
                                         
                         </div>
                         <button class="addbutton" data-product-code=${obj.wands[i].code}>Add to Cart</button>
@@ -80,6 +68,8 @@ $(function(){
                 currentProducts.push({item: addItem, num: "1"});
             }
             localStorage.setItem('items', JSON.stringify(currentProducts));
+            $('.badge').text(currentProducts.length);
+            console.log(currentProducts.length);
         });
     }
 }); // ready
